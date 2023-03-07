9ee56ea9a1e83e6130401c1aa78edf53739a04bd
Adesh Singh Rathore <72265093+AdeshSingh7@users.noreply.github.com>
2023-03-07 15:25:17 +0530

    Update smb-conf.py

--- smb-conf.py
+++ smb-conf.py
@@ -3,7 +3,7 @@ import os
 
 def install_samba():
     os.system(f"sudo apt-get -y update")
-    os.system(f"sudo apt-get -y install samba samba-common-bin")
+    os.system(f"sudo apt-get -y install samba samba-common samba-common-bin")
     #os.system(f"sudo apt-get -y install --reinstall gvfs-backends")
     os.system(f"sudo apt-get -y autoremove")
 
@@ -13,7 +13,7 @@ def create_user(username):
 
 def configure_samba(shared_path, username):
     # Create a backup of the original Samba configuration file
-    os.system(f"sudo cp /etc/samba/smb.conf /etc/samba/smb.conf.bak")
+    os.system(f"sudo mv /etc/samba/smb.conf /etc/samba/smb.conf.bak")
     # Create a new Samba configuration file
     with open(f"/etc/samba/smb.conf", 'w') as f:
         f.write(f"[global]\n")
@@ -23,14 +23,14 @@ def configure_samba(shared_path, username):
         f.write(f"security = user\n")
         f.write(f"map to guest = bad user\n")
         f.write(f"\n")
-        f.write(f"[shared]\n")
+        f.write(f"[Shared]\n")
         f.write(f"path = {shared_path}\n")
         f.write(f"browsable = yes\n")
         f.write(f"writable = yes\n")
         f.write(f"guest ok = yes\n")
         f.write(f"read only = no\n")
     # Create the shared directory
-    os.system(f"sudo mkdir -p {shared_path}")
+    # os.system(f"sudo mkdir -p {shared_path}")
     # Restart the Samba service
     os.system(f"sudo service smbd restart")
 
